e556199247ff91e125fdc3eae3f3631f5fcaa0bc
iamhyc <sudofree@163.com>
2017-07-25 19:55:55 +0800

    first commit

---
+++ kohill.h
@@ -0,0 +1,29 @@
+/*
+ * print.h
+ *
+ *  Created on: Jul 25, 2017
+ *      Author: kohill
+ */
+
+#ifndef SRC_DETECT_FACTORY_KOHILL_H_
+#define SRC_DETECT_FACTORY_KOHILL_H_
+namespace kohill{
+#include <iostream>
+template<typename T>
+void print(T x){
+	std::cout << x << std::endl;
+}
+template<typename T0,typename T1>
+void print(T0 x,T1 y){
+	std::cout << x <<" "<<y<< std::endl;
+}
+template<typename T0,typename T1,typename T2>
+void print(T0 x,T1 y,T2 z){
+	std::cout << x <<" "<<y<< ""<< z<<std::endl;
+}
+};
+
+
+
+
+#endif /* SRC_DETECT_FACTORY_KOHILL_H_ */
