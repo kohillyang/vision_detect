50c16300a565c52829bb0969336b71323bc0a7ce
Unknown <kohillyang@126.com>
2017-08-04 15:26:40 +0800

    add usb_cam support

--- src/detect_factory/main.cpp
+++ src/detect_factory/main.cpp
@@ -9,21 +9,101 @@
 #include "armor_detect_node.h"
 #include <geometry_msgs/PoseStamped.h>
 #include <vision_unit/armor_msg.h>
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <ros/ros.h>
+#include<tf/tf.h>
+#include <boost/thread.hpp>
+#include <boost/atomic.hpp>
+#include <opencv2/opencv.hpp>
+#include "armor_detect.h"
+#include <logical_core/SetGoal.h>
+#include <vision_unit/armor_msg.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <serial_comm/car_speed.h>
+#include "armor_detect.h"
 
-int main(int argc, char **argv)
+#define USB_USB_CAM 0
+#if USB_USB_CAM
+namespace autocar
 {
-
-  ros::init(argc, argv, "armor_detect");
-  try
-  {
-    //Try to detect armor.
-    static autocar::vision_mul::armor_detect_node armor_solver;
-    armor_solver.running();
-  }
-  catch (const std::exception &ex)
-  {
-    std::cout << ex.what() << std::endl;
-  }
-  return 0;
+namespace vision_mul
+{
+class armor_detect;
 }
+}
+
+
+static const std::string OPENCV_WINDOW = "Image window";
+using autocar::vision_mul::armor_detect;
+
+
+
+class ImageConverter {
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	static void imageCb(const sensor_msgs::ImageConstPtr &msg){
+		std::cout << "hello world" << std::endl;
+		static autocar::vision_mul::armor_detect_node *armor_solver = 0;
+		if(!armor_solver){
+			armor_solver = new autocar::vision_mul::armor_detect_node;
+		}
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(msg,
+					sensor_msgs::image_encodings::BGR8);
+		} catch (cv_bridge::Exception &e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+		cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+		armor_solver->detect(cv_ptr->image);
+	}
+public:
+	ImageConverter() :
+			it_(nh_) {
+		std::cout << "hello world" << std::endl;
+		image_sub_ = this->it_.subscribe("/usb_cam/image_raw", 1, ImageConverter::imageCb);
+		cv::namedWindow(OPENCV_WINDOW);
+
+	}
+
+	~ImageConverter() {
+		cv::destroyWindow(OPENCV_WINDOW);
+	}
+};
+
+int main(int argc, char **argv) {
+
+	ros::init(argc, argv, "armor_detect");
+	volatile ImageConverter ic = ImageConverter();
+	ros::spin();
+	return 0;
+}
+#else
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "armor_detect");
+	autocar::vision_mul::armor_detect_node armor_solver;
+    cv::Mat image;
+	cv::VideoCapture capture_camera_forward("/home/kohill/vision_dataset/13.avi");
+//	    cv::VideoCapture capture_camera_forward(0);
+    capture_camera_forward.set(CV_CAP_PROP_FRAME_WIDTH, 800);
+    capture_camera_forward.set(CV_CAP_PROP_FRAME_HEIGHT, 600);
+
+	for(;;){
+	    capture_camera_forward >> image;
+	    armor_solver.detect(image);
+		std::cout<<"Cannot open the camera!"<<std::endl;
+	}
+	ros::spin();
+	return 0;
+}
+#endif
 
